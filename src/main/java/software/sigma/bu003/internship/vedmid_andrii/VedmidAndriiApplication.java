1f83f0316dd41cab1e10db2e7ae62b48419dfbd8
andrii.vedmid <andrii.vedmid@sigma.software>
2022-11-01 11:31:53 +0200

    Init commit

---
+++ src/main/java/software/sigma/bu003/internship/vedmid_andrii/VedmidAndriiApplication.java
@@ -0,0 +1,13 @@
+package software.sigma.bu003.internship.vedmid_andrii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedmidAndriiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedmidAndriiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/software/sigma/bu003/internship/vedmid_andrii/VedmidAndriiApplicationTests.java
@@ -0,0 +1,13 @@
+package software.sigma.bu003.internship.vedmid_andrii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedmidAndriiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
