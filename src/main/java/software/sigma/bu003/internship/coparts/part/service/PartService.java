c53dd57ce40f8acbd82bf7c10f743a5aba605237
andrii.vedmid <andrii.vedmid@sigma.software>
2022-12-23 11:19:49 +0200

    SIB-29 add pagination

--- src/main/java/software/sigma/bu003/internship/coparts/part/service/PartService.java
+++ src/main/java/software/sigma/bu003/internship/coparts/part/service/PartService.java
@@ -1,8 +1,12 @@
 package software.sigma.bu003.internship.coparts.part.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import software.sigma.bu003.internship.coparts.client.CopartsClient;
+import software.sigma.bu003.internship.coparts.part.entity.PageParts;
 import software.sigma.bu003.internship.coparts.part.entity.Part;
 import software.sigma.bu003.internship.coparts.part.service.exception.PartNotFoundException;
 import software.sigma.bu003.internship.coparts.part.repository.PartRepository;
@@ -26,8 +30,12 @@ public class PartService {
         }
     }
 
-    public List<Part> getAllParts() {
-        return partRepository.findAll();
+    public PageParts getAllParts(int page, int size) {
+        Pageable paging = PageRequest.of(page, size);
+
+        Page<Part> pageParts = partRepository.findAll(paging);
+
+        return new PageParts(pageParts);
     }
 
     public Part getPart(String brand, String code) {
